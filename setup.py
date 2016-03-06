cd2e706078a2ce6125eed9fbd7b4b61375b299ae
Chris Luke <chrisy@flirble.org>
2016-03-06 12:37:57 -0500

    Finally got around to adding a LICENSE

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#
+#    Copyright 2016 Chris Luke
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
 
 import os, sys
 
@@ -51,7 +66,7 @@ setup(name='FlirbleDNSServer',
       package_dir = {'FlirbleDNSServer': 'FlirbleDNSServer'},
       scripts = ['dns-server', 'init-rethinkdb', 'update-server'],
       requires = ['dnslib (>=0.9.2)', 'geoip2 (>=2.2.0)', 'lockfile (>=0.12.2)', 'rethinkdb (>=2.2.0)'],
-      license = 'BSD',
+      license = 'Apache-2.0',
       classifiers = [ "Topic :: Internet :: Name Service (DNS)",
                       "Programming Language :: Python :: 2",
                       ],
